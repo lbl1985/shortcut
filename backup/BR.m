b43fa2d8b89af8086a746d537357cf56777e9bfa
binlong <herbert19lee@gmail.com>
2010-11-24 15:57:46 -0500

    Another some change about backup folder under shortcut, just a small change... to see whether I can use the command git add ./backup/* to add multiple files under one folder.

---
+++ backup/BR.m
@@ -0,0 +1,10 @@
+addpath(genpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\cuboids'))
+addpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\cuboids_trial\rep\code\ParInval');
+addpath(genpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\cuboids_trial\rep\code'));
+cd C:\Users\lbl1985\Documents\MATLAB\shortcut
+addpath(pwd); addpath(genpath(pwd));
+go kth
+addpath(genpath(pwd))
+addpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\cuboids_trial\KTH\code\preProcess');
+addpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\cuboids_trial\KTH\code');
+vcm
---
+++ backup/shortcut.m
@@ -0,0 +1,10 @@
+k = pwd;
+cd C:\Users\lbl1985\Documents\MATLAB\shortcut\cvx;
+cvx_setup;
+
+addpath(genpath('C:\Users\lbl1985\Documents\MATLAB\work\BR\toolbox'));
+addpath C:\Users\lbl1985\Documents\MATLAB\work\RIB;
+addpath C:\Users\lbl1985\Documents\MATLAB\work\cali\TOOLBOX_calib;
+
+cd(k);
+clear; clc; close all;
