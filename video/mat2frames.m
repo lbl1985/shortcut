9d7caba7025832bad683ea33da2255395f02e10d
lbl1958_PC <herbert19lee@gmail.com>
2011-02-10 17:17:51 -0500

    Modify the file video/mat2frames.m

--- video/mat2frames.m
+++ video/mat2frames.m
@@ -1,34 +1,48 @@
 function varargout = mat2frames(mat, varargin)
+% Input:
+% mat: Object matrix
+% dest: destination for saving images.
+% filename: it could be the output of rfdatabase, or the saving images file
+% name w/o ext.
+% ext: saving files externsion.
+
 olderpath = pwd;
-if nargin == 3
+if nargin == 4
     dest = varargin{1};
     filename = varargin{2};
+    ext = varargin{3};
     %     movie = varargin{2};
+    %     if nargin < 5
     if ispc
         k = strfind(filename, '\');
     else
         k = strfind(filename, '/');
     end
+    %     end
     %     olderpath = pwd;
     % movie = mmread(filename);
     nFrame = size(mat, ndims(mat));
     varargout{1} = nFrame;
-    nDigtal = 4;
+    nDigtal = 3;
     if ~exist(dest, 'dir')
         mkdir(dest);
     end
     cd(dest);
-    filenameT = filename(k(end) + 1 : end-4);
+    if ~isempty(k)
+        filenameT = filename(k(end) + 1 : end-4);
+    else
+        filenameT = filename;
+    end
     for i = 1 : nFrame
         if ndims(mat) == 3
             I = mat(:, :, i);
         elseif ndims(mat) == 4
             I = mat(:, :, :, i);
-        else 
+        else
             error('We can only handle ndims(M) == 3 or 4');
         end
-%         I = movie.frames(i).cdata;
-        imwrite(I, [filenameT '_' int2str2(i, nDigtal), '.jpg']);
+        %         I = movie.frames(i).cdata;
+        imwrite(I, [filenameT '_' int2str2(i, nDigtal), '.' ext]);
     end
 end
 cd(olderpath)
