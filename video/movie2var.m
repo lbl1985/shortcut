ebd9247c597002fcff2e9914df6f1e8eeb378716
Binlong Li <herbert19lee@gmail.com>
2011-07-15 17:00:21 -0400

    isGray could be [] (empty). The program will automatically decide it, to resever the original format, in case the user is not sure what kind of video it really is

--- video/movie2var.m
+++ video/movie2var.m
@@ -1,9 +1,21 @@
 function mat = movie2var(filename, isGray, siz)
+% isGray could be []. The program will reserve the original format
 if ~ispc
     currPath = pwd;
 end
 video = mmread(filename);
 n = length(video.frames);
+
+if isempth(isGray)
+    % user dont' specifically require for gray or rgb. Then save it as the
+    % original format
+    sampleFrame = video.frames(1).cdata;
+    if ndims(sampleFrame) > 3
+        isGray = 0;
+    else
+        isGray = 1;
+    end
+end
 
 if ~isGray
     mat = uint8(zeros(video.height * siz, video.width * siz ,3, n));    
